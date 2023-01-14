f9ebc82c7e063b9a78ddb1c15e6107606726a006
jaum-fs <martins_joao@id.uff.br>
2023-01-14 00:11:21 -0300

    conectando ao mongoDB

---
+++ src/main/java/com/projeto/workshopmongo/resources/UserResource.java
@@ -0,0 +1,26 @@
+package com.projeto.workshopmongo.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projeto.workshopmongo.domain.User;
+import com.projeto.workshopmongo.services.UserService;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+	@Autowired
+	private UserService service;
+	
+	@GetMapping
+	public ResponseEntity<List<User>> findAll(){
+		return ResponseEntity.ok().body(service.findAll());
+	}
+	
+}
